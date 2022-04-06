7d02375f6aeb2ce239b0c12d116939c79c4bef77
karam <karamkaku2000@gmail.com>
2022-04-06 05:25:37 +0200

    add auth component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-
+import Auth from "../components/auth"
 const Home: NextPage = () => {
   return (
     <div>
@@ -9,18 +9,7 @@ const Home: NextPage = () => {
        
       </Head>
 
-      <div>
-      <button className='btn-primary'>
-        click me
-      </button>
-      <button className='btn-secondary'>
-        click me
-      </button>
-      <input type="text" className='formInput-medium' placeholder='first name'/>
-      <p className='text-small text-secondary'>
-      ;Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nulla, aliquam explicabo eos odit rerum dolorem consequatur quidem, hic nihil magnam, consectetur tempore tenetur dolorum dolores eum quam soluta deleniti!
-      </p>
-      </div>
+     <Auth/>
     </div>
   )
 }
