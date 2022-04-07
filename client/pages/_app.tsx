7bc00361696161ce223d54208f4a07216259bf4e
karam <karamkaku2000@gmail.com>
2022-04-07 03:15:57 +0200

    add authContextProvider to _app.jsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,11 +2,12 @@ import '../styles/globals.css';
 import '../styles/button.css';
 import '../styles/forminput.css';
 import '../styles/text.css';
-
 import Head from 'next/head';
+import {AuthContextProvider} from "../components/auth/context/authContext";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return( 
     <div  className="light-theme">
       <Head>
@@ -14,7 +15,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;700&display=swap" rel="stylesheet"/>
       </Head>
+      <AuthContextProvider>
       <Component {...pageProps}/>
+      </AuthContextProvider>
 
     </div>
   )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Auth from "../components/auth"
+import Auth from "../components/auth";
+import {useAuthContext} from "../components/auth/context/authContext";
 const Home: NextPage = () => {
+  const {setAuth} = useAuthContext()
   return (
     <div>
       <Head>
         <title>live streaming app</title>
        
       </Head>
-
+     <button onClick={_=>setAuth("register")}>register</button>
+     <button onClick={_=>setAuth("login")}>login</button>
      <Auth/>
     </div>
   )
