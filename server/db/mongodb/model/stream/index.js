da4faabebfe1c197d3339d545c64ca307944a657
karam <karamkaku2000@gmail.com>
2022-04-20 13:50:04 +0200

    add date to model

--- server/db/mongodb/model/stream/index.js
+++ server/db/mongodb/model/stream/index.js
@@ -6,7 +6,8 @@ const StreamSchema = mongoose.Schema({
     title:String,
     tags:[{type:String}],
     type:String,
-    streamLink:{type:String,validate:/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i}
+    streamLink:{type:String,validate:/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i},
+    date: Date 
 })
 
 
