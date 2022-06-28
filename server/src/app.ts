0226e4663489b87d9cedc3bde83c79b9718a0c72
karam koujan <karamkaku2000@gmail.com>
2022-06-29 01:33:26 +0200

    set up node js typescript envirement

---
+++ server/src/app.ts
@@ -0,0 +1,9 @@
+"use strict";
+function sayMyName(name:string) {
+  if (name === "Heisenberg") {
+    console.log("You're left 👍");
+  } else {
+    console.log("You're wrong 👎");
+  }
+}
+sayMyName("Heisenberg");
