f2f6d996e3dad2e5a9528dddc0d6f4545e13e492
karam koujan <karamkaku2000@gmail.com>
2022-07-14 00:51:36 +0200

    chore: add refresh token route

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,26 @@ import { UserResolver } from "./userResolver";
 import "reflect-metadata";
 import {buildSchema} from "type-graphql";
 import {connect} from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const {dbConnection} = require("./config/"); 
+import refreshToken from "./refreshToken";
+
 (async()=>{
 const app = express() 
 const port = process.env.NODE_ENV || 8000 ; 
 
 const apolloServer = new ApolloServer({
   schema : await buildSchema({
-     resolvers : [UserResolver],
-  })
+     resolvers : [UserResolver]  
+    }),
+  context : ({req,res})=>({req,res}),
 
 })
+app.use(cookieParser())
 apolloServer.start().then(()=>{
-	
-apolloServer.applyMiddleware({app})
+apolloServer.applyMiddleware({app,cors:{origin:"https://studio.apollographql.com",credentials:true}})
+   app.get("/refresh_token",refreshToken)
    console.log(dbConnection)
    connect(dbConnection,()=>console.log("db is connected successfully"))
    app.listen(port,()=>console.log(`server is listening on port ${port}`))
